2fb3be7fa47a314720b3c645d0fbd25f46e0328c
Matthias Bolte <matthias@tinkerforge.com>
2022-05-10 16:36:49 +0200

    go: Fix format string

--- software/examples/go/example_simple.go
+++ software/examples/go/example_simple.go
@@ -21,13 +21,13 @@ func main() {
 	// Get current date and time.
 	year, month, day, hour, minute, second, centisecond, weekday, _ := rtc.GetDateTime()
 
-	fmt.Printf("Year: \n", year)
-	fmt.Printf("Month: \n", month)
-	fmt.Printf("Day: \n", day)
-	fmt.Printf("Hour: \n", hour)
-	fmt.Printf("Minute: \n", minute)
-	fmt.Printf("Second: \n", second)
-	fmt.Printf("Centisecond: \n", centisecond)
+	fmt.Printf("Year: %d\n", year)
+	fmt.Printf("Month: %d\n", month)
+	fmt.Printf("Day: %d\n", day)
+	fmt.Printf("Hour: %d\n", hour)
+	fmt.Printf("Minute: %d\n", minute)
+	fmt.Printf("Second: %d\n", second)
+	fmt.Printf("Centisecond: %d\n", centisecond)
 
 	if weekday == real_time_clock_bricklet.WeekdayMonday {
 		fmt.Println("Weekday: Monday")
@@ -47,7 +47,7 @@ func main() {
 
 	// Get current timestamp.
 	timestamp, _ := rtc.GetTimestamp()
-	fmt.Printf("Timestamp:  ms\n", timestamp)
+	fmt.Printf("Timestamp: %d ms\n", timestamp)
 
 	fmt.Print("Press enter to exit.")
 	fmt.Scanln()
